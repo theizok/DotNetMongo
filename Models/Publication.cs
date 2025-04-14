c9b18fb5c9c6edcd644e90d1a4a711111d85d961
theizok <ramirezizok@gmail.com>
2025-04-14 15:13:08 -0500

    First Commit

---
+++ Models/Publication.cs
@@ -0,0 +1,14 @@
+public class Publication {
+
+private int publicationId {get; set;};
+private int authorId {get;set;}:
+private string content {get; set;};
+private DateTime date {get; set;};
+
+
+public Publication (string content, DateTime date){
+    this.content = content
+    this.date = date 
+}
+
+}    
